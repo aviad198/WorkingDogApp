85a576879857e0c542deef52729552d5f474749b
aviad198 <aviad198@gmail.com>
2020-09-23 20:15:22 +0300

    update of pictuer and file name

--- TimeApp.java
+++ TimeApp.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Timer;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -41,7 +42,9 @@ public class TimeApp implements ActionListener, WindowListener {
 	DateTimeFormatter myFormatDate;
 	DateTimeFormatter myFormatTime;
 	Boolean startWork;
-
+	String curFileDate;
+	File myTimes;
+	
 	public TimeApp() {
 		startWork = true;
 		createFile();
@@ -49,18 +52,21 @@ public class TimeApp implements ActionListener, WindowListener {
 		buildFrame(); 
 		
 	}
-
+		/*Creates a file with the month name 
+		 * 
+		 */
 	private void createFile() {
-		new File("My Work");
-
+		/*try creating the file*/
 		try {
 			String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 			curDate = LocalDate.now();
-			File myTimes = new File(monthNames[curDate.getMonthValue()]+"-Times.txt");
-			
+			myTimes = new File(monthNames[curDate.getMonthValue()-1]+"-Times.txt");
+			/*
+			 * if file didnt exist creats a new one with header of date start time and end time  
+			 */
 			if (myTimes.createNewFile()) {
 				System.out.println("File created: " + myTimes.getName());
-				myWriter = new FileWriter("timeStamps.txt");
+				myWriter = new FileWriter(myTimes.getName());
 				myWriter.write("Date\t\tStart Time\tEnd Time\n");
 				myWriter.close();
 			} else {
@@ -73,16 +79,22 @@ public class TimeApp implements ActionListener, WindowListener {
 		}		
 	}
 
+		/*
+		 * setting the date and time format
+		 */
 	private void setTimeFormat() {
 		myFormatDate = DateTimeFormatter.ofPattern("dd.MM.yyyy");// change format that only decade appeares in year
 		myFormatTime = DateTimeFormatter.ofPattern("HH:mm");// 		
 	}
 
+	/*
+	 * Builds the frame - the user interface
+	 */
 	private void buildFrame() {
 		appFrame = new JFrame("Working Time");
 		appFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Aviad\\Desktop\\MyAppImg.png"));
 		appFrame.setVisible(true);
-		startEndBtn = new JButton("");
+		startEndBtn = new JButton();
 		startEndBtn.setBackground(new Color(30, 144, 255));
 		startEndBtn.setIcon(new ImageIcon("C:\\Users\\Aviad\\eclipse-workspace\\WorkTime\\Aviadprogram-03.png"));
 		
@@ -92,6 +104,7 @@ public class TimeApp implements ActionListener, WindowListener {
 
 		appFrame.addWindowListener(this);
 		appFrame.setSize(258, 237);
+		//set the location to Bottom right of screen
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		appFrame.setLocation(dim.width - appFrame.getSize().width - 200, dim.height - appFrame.getSize().height - 40);
 		appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -99,6 +112,7 @@ public class TimeApp implements ActionListener, WindowListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
+		//Button pressed
 		if (e.getSource() == startEndBtn) {
 			boolean newDate = false;
 			myDateObj = LocalDate.now();
@@ -107,23 +121,24 @@ public class TimeApp implements ActionListener, WindowListener {
 				curDate = myDateObj;
 				newDate = true;
 			}
-			// myFormatObj = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm\n");//change
-			// format that only decade appeares in year
+			
 			try {
-				myWriter = new FileWriter("timeStamps.txt", true);
+				myWriter = new FileWriter(myTimes.getName(), true);
 				if (newDate) {
 					myWriter.write(curDate.format(myFormatDate));
 				} else
 					myWriter.write("\t");
-
-				// System.out.println(myDateObj.format(myFormatObj));
-
-				// System.out.println("Successfully wrote to the file.");
+				//if its the start of work
 				if (startWork) {
+					/*add timer for over work
+					 *Timer timer = new Timer();
+					/*timer.schedule(task, delay);
+					 */
+					
 					myWriter.write("\t" + myTimeObj.format(myFormatTime));
 					startEndBtn.setIcon(new ImageIcon("C:\\Users\\Aviad\\eclipse-workspace\\WorkTime\\Aviadprogram-05.png"));
-	
 					startWork = false;
+					//if its the end of work
 				} else {
 					myWriter.write("\t" + myTimeObj.format(myFormatTime) + "\n");
 					startEndBtn.setIcon(new ImageIcon("C:\\Users\\Aviad\\eclipse-workspace\\WorkTime\\Aviadprogram-03.png"));
@@ -147,11 +162,14 @@ public class TimeApp implements ActionListener, WindowListener {
 	}
 
 	@Override
+	/*
+	 * if window closed without pressing the end work Button - add the end stamp of current time 
+	 */
 	public void windowClosing(WindowEvent e) {
 		if (!startWork) {
 			myTimeObj = LocalTime.now();
 			try {
-				myWriter = new FileWriter("timeStamps.txt", true);
+				myWriter = new FileWriter(myTimes.getName(), true);
 				myWriter.write("\t\t" + myTimeObj.format(myFormatTime) + "\n");
 				myWriter.close();
 				System.out.println("Successfully wrote to the file.");
